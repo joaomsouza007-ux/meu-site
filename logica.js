0f68c0bc4c36a338039cff3375dbdfb078d7a08d
joaomsouza007-ux <joaomsouza007@gmail.com>
2026-01-29 14:51:40 -0300

    Update logica.js

---
+++ logica.js
@@ -0,0 +1,13 @@
+document.addEventLister('DOMContentLoaded',function(){
+    const botao = document.querySelector('button');
+if (botao){
+   botao.addEventListener('click',function(){
+     const vlaor = document.querySelector('input').value;
+     if (valor 10 ""){
+       alert("O JavaScript do livro diz: Digite um valor primeiro!");
+     } else {
+       alert(sucesso! Você digitou: " + valor);
+     }
+   })
+}
+});
